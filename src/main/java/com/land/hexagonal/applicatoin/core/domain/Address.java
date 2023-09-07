41b6f52590436cf95f8fe68143de1ea4a30a7969
landson90 <randelsilva1990@gmail.com>
2023-09-07 12:45:19 -0300

    criando classes do domain customer e address

---
+++ src/main/java/com/land/hexagonal/applicatoin/core/domain/Address.java
@@ -0,0 +1,8 @@
+package com.land.hexagonal.applicatoin.core.domain;
+
+public class Address {
+
+    private String street;
+    private String city;
+    private String state;
+}
---
+++ src/main/java/com/land/hexagonal/applicatoin/core/domain/Customer.java
@@ -0,0 +1,10 @@
+package com.land.hexagonal.applicatoin.core.domain;
+
+public class Customer {
+
+    private String id;
+    private String name;
+    private Address address;
+    private String cpf;
+    private Boolean isValidCpf;
+}
