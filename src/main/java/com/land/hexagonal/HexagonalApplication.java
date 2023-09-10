b47df81653ca3ff3c50fe1dc5558a708cd2abf09
landson90 <randelsilva1990@gmail.com>
2023-09-10 20:30:29 -0300

    criando o fluxo para usar o fieng client

--- src/main/java/com/land/hexagonal/HexagonalApplication.java
+++ src/main/java/com/land/hexagonal/HexagonalApplication.java
@@ -1,8 +1,11 @@
 package com.land.hexagonal;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HexagonalApplication {
 
--- src/main/java/com/land/hexagonal/applicatoin/core/usecase/InsertCustomerUseCase.java
+++ src/main/java/com/land/hexagonal/applicatoin/core/usecase/InsertCustomerUseCase.java
@@ -1,6 +1,23 @@
 package com.land.hexagonal.applicatoin.core.usecase;
 
+import com.land.hexagonal.applicatoin.core.domain.CustomerDomain;
+import com.land.hexagonal.applicatoin.ports.out.FindAddressByZipCodeOutputPort;
+import com.land.hexagonal.applicatoin.ports.out.InsertCustomerDomainOutputPort;
+
 public class InsertCustomerUseCase {
 
+    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
+    private final InsertCustomerDomainOutputPort insertCustomerOutputPort;
 
+    public InsertCustomerUseCase(FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort, InsertCustomerDomainOutputPort insertCustomerOutputPort) {
+        this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
+        this.insertCustomerOutputPort = insertCustomerOutputPort;
+    }
+
+    void insert(CustomerDomain customerDomain, String zipCode) {
+        var address = this.findAddressByZipCodeOutputPort.find(zipCode);
+        customerDomain.setAddress(address);
+        this.insertCustomerOutputPort.insert(customerDomain);
+
+    }
 }
