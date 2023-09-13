6c84ac976d15e51844bc5af12509a7eaa3fab881
landson90 <randelsilva1990@gmail.com>
2023-09-13 14:39:42 -0300

    configurado os kafka

---
+++ src/main/java/com/land/hexagonal/adapters/in/consumer/message/CustomerMessage.java
@@ -0,0 +1,17 @@
+package com.land.hexagonal.adapters.in.consumer.message;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerMessage {
+    private String id;
+    private String name;
+    private String zipCode;
+    private String cpf;
+    private Boolean isValidCpf;
+}
